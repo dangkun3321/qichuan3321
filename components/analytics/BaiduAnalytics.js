ea87f343838c82fd7bf6b658c6418e36d8ebf3d5
liuzhen <zentodolist@gmail.com>
2022-04-10 19:32:12 +0800

    fix baidu analytics id

--- components/analytics/BaiduAnalytics.js
+++ components/analytics/BaiduAnalytics.js
@@ -9,7 +9,7 @@ const BAScript = () => {
           var _hmt = _hmt || [];
           (function() {
             var hm = document.createElement("script");
-            hm.src = "https://hm.baidu.com/hm.js?${siteMetadata.analytics.googleAnalyticsId}";
+            hm.src = "https://hm.baidu.com/hm.js?${siteMetadata.analytics.baiduAnalyticsId}";
             var s = document.getElementsByTagName("script")[0];
             s.parentNode.insertBefore(hm, s);
           })();
