1acefbcf09d4b8bdf6b9b8d8c801c40516f61ff1
Seven Du <dujinfang@x-y-t.cn>
2022-05-17 15:15:28 +0800

    remove docs link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,6 @@ const headerNavLinks = [
   // { href: '/solutions', title: 'Solutions', subHref: { href: '/products', title: 'Products' } },
   // { href: '/pricing', title: 'Pricing' },
   { href: '/pages/docs', title: 'Resources' },
-  { href: '/pages/docs', title: 'Docs' },
   // { href: '/pages/docs', title: 'Docs', subHref: { href: '/pages', title: 'Pages' } },
   // { href: '/case', title: 'Case' },
   { href: 'https://x-y-t.cn', title: 'About Us' },
