bb1b9e1de35ec3cea00007868e8167e3eb987fb3
liuzhen <zentodolist@gmail.com>
2022-05-18 08:31:35 +0800

    update header

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -2,7 +2,27 @@ const headerNavLinks = [
   // { href: '/products', title: 'Products', subHref: { href: '/products', title: 'Products' } },
   // { href: '/solutions', title: 'Solutions', subHref: { href: '/products', title: 'Products' } },
   // { href: '/pricing', title: 'Pricing' },
-  { href: '/pages/docs', title: 'Resources' },
+  {
+    title: 'Resources',
+    subHref: [
+      {
+        href: '/docs/xswitch-user.html',
+        title: 'User Manual',
+      },
+      {
+        href: '/pages/docs',
+        title: 'Document',
+      },
+      {
+        href: 'https://git.xswitch.cn/xswitch/xswitch',
+        title: 'Git Code',
+      },
+      {
+        href: 'https://github.com/rts-cn',
+        title: 'Github Code',
+      },
+    ],
+  },
   // { href: '/pages/docs', title: 'Docs', subHref: { href: '/pages', title: 'Pages' } },
   // { href: '/case', title: 'Case' },
   { href: 'https://x-y-t.cn', title: 'About Us' },
